aba53839cbe3de8d461d801c9980777e1ca4553e
Danish Goel <danishdgoel@gmail.com>
2015-11-20 12:10:56 +0530

    added missing tileCoordinates file

---
+++ core/src/com/ai/chainreaction/tileCoordinates.java
@@ -0,0 +1,24 @@
+package com.ai.chainreaction;
+
+/**
+ * Created by Danish on 20-Nov-15.
+ */
+
+public class tileCoordinates {
+    int row;
+    int col;
+
+    public tileCoordinates(int row,int col)
+    {
+        this.row=row;
+        this.col=col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+}
