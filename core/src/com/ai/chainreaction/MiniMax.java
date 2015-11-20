3480031bcd5cdecce6d520528a6eb860f72b7d66
Danish Goel <danishdgoel@gmail.com>
2015-11-20 21:39:38 +0530

    heuristics

--- core/src/com/ai/chainreaction/MiniMax.java
+++ core/src/com/ai/chainreaction/MiniMax.java
@@ -33,7 +33,7 @@ public class MiniMax {
             for (int col = 0; col < numColumns; col++) {
                 Tile til = tiles[row][col];
                 if (til.color == Tile.EMPTY || til.color == color) {
-                    TileCoordinate c = new TileCoordinate(row,col);
+                    TileCoordinate c = new TileCoordinate(row, col);
                     orbPlacements.put(til, c);
                 }
             }
@@ -84,23 +84,96 @@ public class MiniMax {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 Tile tile = grid[i][j];
-                tile.color=Tile.EMPTY;
+                tile.color = Tile.EMPTY;
                 tile.numOrbs = 0;
             }
         }
-        for(TileData each : filledTiles)
-        {
+        for (TileData each : filledTiles) {
             Tile tile = grid[each.row][each.col];
             tile.color = each.color;
             tile.numOrbs = each.numOrbs;
         }
     }
 
-    int getHeuristic(Tile[][] tiles, int row, int col) {
+    int getHeuristic(Tile[][] tiles, int color) {
 
         //bbvfdvdf
+        int myOrbs = 0;
+        int enemyOrbs = 0;
+        int score = 0;
+        boolean vurnerable=true;
+        for (int row = 0; row < tiles.length; row++) {
+            for (int col = 0; col < tiles[0].length; col++) {
+                if (color == Tile.BLUE) {
+                    myOrbs++;
+                    for (int i = -1; i <= 1; i++) {
+                        for (int j = -1;j<=1;j++ )
+                        {
+                            if(i==j)
+                                continue;
+                            if(i!=0 || j!=0)
+                                continue;
+                            if(checkExistance(tiles.length,tiles[0].length,row+i,col+j))
+                            {
+                                Tile tile = tiles[row+i][col+j];
+                                if(tile.color!=color)   //surrounding enemy neighbours
+                                {
+                                    if(tile.numOrbs==tile.threshold-1)
+                                    {
+                                        score-=5-tile.threshold;
+                                        vurnerable=false;
+                                    }
+                                }
+                            }
+                        }
+                    }
 
-        return 0;
+                    if(vurnerable)
+                    {
+                        Tile currentTile=tiles[row][col];
+                        if(currentTile.threshold==3)
+                        {
+                            score+=2;
+                        }
+                        else if(currentTile.threshold==2)
+                        {
+                            score+=3;
+                        }
+                        else if(currentTile.numOrbs+1==currentTile.threshold)
+                        {
+                            score+=2;
+                        }
+                    }
+                } else if (color == Tile.RED) {
+                    enemyOrbs++;
+                }
+
+
+            }
+        }
+        if (myOrbs > 1 && enemyOrbs == 0) {
+            score += 1000;    //win case
+        } else if (myOrbs == 0 && enemyOrbs > 1) {
+            score -= 1000;    //lose case
+        }
+
+
+        return score;
+    }
+
+    public int  chains(Tile[][] tiles)
+    {
+        int numRows=tiles.length;
+        int numColumns=tiles[0].length;
+        boolean visited[][]=new boolean[numRows][numColumns];
+        for(int i=0;i<numRows;i++)
+        {
+            for(int j=0;j<numColumns;j++)
+            {
+                Tile tile = tiles[i][j];
+
+            }
+        }
     }
 
     public class TileData {
@@ -125,13 +198,20 @@ public class MiniMax {
             for (int j = 0; j < cols; j++) {
                 Tile tile = grid[i][j];
                 if (tile.color != Tile.EMPTY) {
-                    filledTiles.add(new TileData(i,j,tile.numOrbs,tile.color));
+                    filledTiles.add(new TileData(i, j, tile.numOrbs, tile.color));
                 }
             }
         }
         return filledTiles;
     }
 
+    public boolean checkExistance(int rows, int cols, int x, int y) {
+        if (x < 0 || x >= rows)
+            return false;
+        if (y < 0 || y >= cols)
+            return false;
+        return true;
+    }
 
 
 }
