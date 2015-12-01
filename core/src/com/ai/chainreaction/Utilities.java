218ab45cc68bb5fb9733e935d546fd50fb701f07
Pranav Chadha <pranavchadha97@gmail.com>
2015-12-01 22:53:41 +0530

    ClickTile and checkwinner added to Utilitites

--- core/src/com/ai/chainreaction/Utilities.java
+++ core/src/com/ai/chainreaction/Utilities.java
@@ -1,9 +1,13 @@
 package com.ai.chainreaction;
 
+import com.badlogic.gdx.Gdx;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -19,6 +23,12 @@ public class Utilities {
 
         public int row;
         public int col;
+
+        @Override
+        public boolean equals(Object obj) {
+            Pos p = (Pos) obj;
+            return this.row==p.row && this.col==p.col;
+        }
     }
 
     public static List<Pos> getPlayablePositions(Tile[][] grid, int turn) {
@@ -124,6 +134,105 @@ public class Utilities {
             }
         }
         return grid;
+    }
+
+    public static int checkWinnerIfExists(int[][] grid) {
+        boolean foundRed = false;
+        boolean foundBlue = false;
+        int count = 0;
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                int tile = grid[row][col];
+                if (tile == 0)
+                    continue;
+                count++;
+                if (tile / Math.abs(tile) == Tile.BLUE)
+                    foundBlue = true;
+                if (tile / Math.abs(tile) == Tile.RED)
+                    foundRed = true;
+            }
+        }
+        if (count < 2)
+            return Tile.EMPTY;
+        if (foundBlue && foundRed)
+            return Tile.EMPTY;
+        if (foundBlue)
+            return Tile.BLUE;
+        if (foundRed)
+            return Tile.RED;
+        return Tile.EMPTY;
+    }
+
+
+
+
+    public static void clickTile(int[][] grid, int color, int currentRow, int currentColumn) {
+
+        int totalRows = grid.length;
+        int totalColumns = grid[0].length;
+        Queue<TileCoordinate> tilesToBeClicked = new LinkedList<TileCoordinate>();
+        TileCoordinate start = new TileCoordinate(currentRow, currentColumn);
+        tilesToBeClicked.add(start);
+        Gdx.app.log("start", "r:" + currentRow + " c:" + currentColumn + "color:" + color);
+        int count = 0;
+        while (!tilesToBeClicked.isEmpty()) {
+            Gdx.app.log("moves", "qSize: " + tilesToBeClicked.size());
+            if (count % 1 == 0) {
+                if (checkWinnerIfExists(grid) != Tile.EMPTY) {
+                    return;
+                }
+            }
+            count++;
+
+            TileCoordinate clickThisTileCoordinate = tilesToBeClicked.remove();
+            Gdx.app.log("minimax", "x" + currentRow + "y" + currentColumn);
+            Gdx.app.log("minimax", "x" + clickThisTileCoordinate.row + "y" + clickThisTileCoordinate.col);
+            Queue<TileCoordinate> returnedTiles = explodedTiles(grid, color, clickThisTileCoordinate.row, clickThisTileCoordinate.col);
+            if (returnedTiles.size() > 0) {
+                tilesToBeClicked.addAll(returnedTiles);
+
+            }
+        }
+        Gdx.app.log("start complete", ChainReaction.debug + " r:" + currentRow + " c:" + currentColumn);
+    }
+
+    private static Queue<TileCoordinate> explodedTiles(int[][] grid, int color, int currentRow, int currentColumn) {
+        Gdx.app.log("explode", ChainReaction.debug + " debug");
+        int totalRows = grid.length;
+        int totalColumns = grid[0].length;
+        int cur = grid[currentRow][currentColumn];
+        cur = Math.abs(cur) + 1;
+        cur *= color;
+        grid[currentRow][currentColumn] = cur;
+
+        Queue<TileCoordinate> tilesToBeClicked = new LinkedList<TileCoordinate>();
+
+        int threshold = Tile.getThreshold(totalRows, totalColumns, currentRow, currentColumn);
+        if (Math.abs(cur) >= threshold) {
+            grid[currentRow][currentColumn] = (Math.abs(cur) % threshold) * color;
+
+            if (Tile.checkExistance(totalRows, totalColumns, currentRow, currentColumn - 1)) {
+                int leftColumn = currentColumn - 1;
+                TileCoordinate leftTile = new TileCoordinate(currentRow, leftColumn);
+                tilesToBeClicked.add(leftTile);
+            }
+            if (Tile.checkExistance(totalRows, totalColumns, currentRow, currentColumn + 1)) {
+                int rightColumn = currentColumn + 1;
+                TileCoordinate rightTile = new TileCoordinate(currentRow, rightColumn);
+                tilesToBeClicked.add(rightTile);
+            }
+            if (Tile.checkExistance(totalRows, totalColumns, currentRow - 1, currentColumn)) {
+                int topRow = currentRow - 1;
+                TileCoordinate topTile = new TileCoordinate(topRow, currentColumn);
+                tilesToBeClicked.add(topTile);
+            }
+            if (Tile.checkExistance(totalRows, totalColumns, currentRow + 1, currentColumn)) {
+                int bottomRow = currentRow + 1;
+                TileCoordinate bottomTile = new TileCoordinate(bottomRow, currentColumn);
+                tilesToBeClicked.add(bottomTile);
+            }
+        }
+        return tilesToBeClicked;
     }
 
 }
